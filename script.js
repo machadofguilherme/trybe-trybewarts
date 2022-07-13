8d82ab8ca4ef82901ab48bc503ed5dc94d9a3fb0
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-13 18:11:07 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,17 @@
+// function validacaoLogin() {
+//   const email = window.document.querySelector('input#email');
+//   const senha = window.document.querySelector('input#password');
+//   if (email === 'tryber@teste.com' && senha === '123456') {
+//     // alert('Olá, Tryber!');
+//   } else {
+//     // alert('Email ou senha inválidos.');
+//   }
+//   return console.log(typeof senha);
+// }
+// const botaoLogin = window.document.querySelector('button#btn-login');
+// botaoLogin.addEventListener('click', validacaoLogin);
+// const senha = window.document.querySelector('input#fieldSenha');
+// const email = window.document.querySelector('input#fieldEmail');
+// email.style.border = '1px solid blue';
+// senha.style.border = '1px solid blue';
+// console.log(senha);
