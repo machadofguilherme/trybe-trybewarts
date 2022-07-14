39d44c976aa62f2facb67dab331400142ececeec
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-14 00:10:11 -0300

    Requisito 18.

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-function validacaoLogin() {
-  const campoEmail = document.querySelector('input#fieldEmail');
-  // const campoSenhaTexto = document.querySelector('input#fieldSenha').value;
-  // const senha = parseInt(campoSenhaTexto, 10);
-  // if (campoEmail === 'tryber@teste.com' && senha === 123456) {
-  //   alert('Olá, Tryber!');
-  // } else {
-  //   alert('Email ou senha inválidos.');
-  // }
-  console.log(campoEmail.value);
-  // console.log(senha);
-}
 const botaoLogin = document.querySelector('button#btn-login');
 botaoLogin.addEventListener('click', validacaoLogin);
+const submitBtn = document.getElementById('submit-btn');
+const submitCheck = document.getElementById('agreement');
+
+submitCheck.addEventListener('click', () => {
+  if (submitCheck.checked === true) {
+    submitBtn.removeAttribute('disabled');
+  } else {
+    submitBtn.setAttribute('disabled', '');
+  }
+});
