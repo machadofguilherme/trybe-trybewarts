6779670c3ca3f5d3d2e79df2bd7d833d7e2df7e1
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-14 00:44:54 -0300

    Requisito 3. ;)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const fieldEmail = document.getElementById('fieldEmail');
+const fieldSenha = document.getElementById('fieldSenha');
+const btnLogin = document.getElementById('btn-login');
 const submitBtn = document.getElementById('submit-btn');
 const submitCheck = document.getElementById('agreement');
 
@@ -6,5 +9,14 @@ submitCheck.addEventListener('click', () => {
     submitBtn.removeAttribute('disabled');
   } else {
     submitBtn.setAttribute('disabled', '');
+  }
+});
+
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (fieldEmail.value === 'tryber@teste.com' && fieldSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
   }
 });
